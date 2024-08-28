a81c6ef268ead2eed4825427ffd1b71cc8ec7aba
YersonMv <YersonMv@DESKTOP-ALJGDP9>
2024-08-28 11:00:24 -0500

    Agregar archivos de proyecto.

---
+++ EmployeeManagement/App_Start/BundleConfig.cs
@@ -0,0 +1,49 @@
+﻿using System.Data;
+using System.Web;
+using System.Web.DynamicData;
+using System.Web.Optimization;
+using System.Web.UI.WebControls;
+
+namespace EmployeeManagement
+{
+    public class BundleConfig
+    {
+        // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información sobre los formularios.  De esta manera estará
+            // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+
+            //Bundle creados
+
+            bundles.Add(new StyleBundle("~/plugin/css").Include(
+                     "~/Content/datatable/css/dataTables.jqueryui.min.css",
+                     "~/Content/fontawesome/css/all.css",
+                     "~/Content/datatable/css/buttons.dataTables.min.css"
+                     ));
+
+            bundles.Add(new StyleBundle("~/plugin/js").Include(
+                    "~/Content/datatable/js/dataTables.jqueryui.min.js",
+                    "~/Content/fontawesome/js/all.js",
+                    "~/Content/datatable/js/dataTables.buttons.min.js"
+                    ));
+        
+
+        }
+    }
+}
---
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+using EmployeeManagement.Models; //Se importa el modelo para poderlo utilizar.
+
+namespace EmployeeManagement.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetEmployees() //Traer todos los Employees
+        {       
+            List<Employee> myEmployees = new List<Employee>(); //Creacion de lista del tipo de modelo a usar.
+            
+            //Modelo de entidades creado por entity framework que accede a la base de datos.
+            using (EmployeeDBEntities db = new EmployeeDBEntities())
+            {
+                myEmployees = (from Employee in db.Employee 
+                               select Employee).ToList(); //Consulta LINQ para llamar los datos y guardarlos en la lista.
+
+                return Json(new {data= myEmployees }, JsonRequestBehavior.AllowGet); //Para el dataTable es necesario pasarle un atributo llamado data
+            }       
+        }
+
+        public JsonResult GetEmployee(int id) {  //Traer 1 solo Employee
+        
+            Employee myEmployee = new Employee();
+
+            using (EmployeeDBEntities db = new EmployeeDBEntities()) 
+            {
+                myEmployee = (from Employee in db.Employee
+                              where Employee.Id == id  //Consulta LINQ para obtener un Employee segun el Id
+                              select Employee).FirstOrDefault();  //Seleccione el primero que encuentre.
+
+                
+            }
+            return Json( myEmployee , JsonRequestBehavior.AllowGet); //se retorna lo obtenido de la consulta. aqui ya no es necesario mandar el atributo, sino el objeto.
+        }
+        [HttpPost]
+        public JsonResult UpdateEmployees(Employee oEmployee) //oEmployee es el objeto que recibe desde el metodo ajax.
+        {
+            bool respuesta = true; //Controla si fue satisfactoria la operación o no.
+            try
+            {
+                if (oEmployee.Id == 0) //Si el id es ==0 Es un nuevo Employee
+                {
+
+                    using (EmployeeDBEntities db = new EmployeeDBEntities())
+                    {
+                        db.Employee.Add(oEmployee);  //Se añade el objeto a la DDBB
+                        db.SaveChanges(); //Guardar los cambios efectuados.
+                    }
+
+                }
+                else
+                {
+                    using (EmployeeDBEntities db = new EmployeeDBEntities())
+                    {
+                        Employee tempEmployee = (from Employee in db.Employee
+                                                 where Employee.Id == oEmployee.Id
+                                                 select Employee).FirstOrDefault();  //Consulta LINQ para obtener un Employee segun el Id
+
+                        tempEmployee.Name = oEmployee.Name;
+                        tempEmployee.Position = oEmployee.Position;  //Se añaden los valores a la db.
+                        tempEmployee.Office = oEmployee.Office;
+                        tempEmployee.Salary = oEmployee.Salary;
+                        db.SaveChanges(); //Es IMPORTANTE tener bien configurada la PK.
+
+                    }
+
+                }
+            }
+            catch
+            {
+                respuesta = false;
+
+            }
+            return Json(new { resultado = respuesta }, JsonRequestBehavior.AllowGet); //Nos devuelve true si todo sale bien o false si existe alguna excepción
+
+        }
+
+        public JsonResult DeleteEmployee(int id) { 
+        
+            bool respuesta = true;  //Nos muestra si es satisfactoria o no la operacion.
+
+            try
+            {
+                using (EmployeeDBEntities db= new EmployeeDBEntities())
+                {
+                    Employee oEmploye = new Employee();
+
+                    oEmploye= (from Employee in db.Employee.Where(e => e.Id == id)  //Manera distinta de obtener el Employee por el Id (Funcion flecha o lambda )
+                               select Employee).FirstOrDefault();
+                    db.Employee.Remove(oEmploye); //Eliminacion del objeto
+                    db.SaveChanges(); //Guardar cambios.
+
+                }
+            } 
+            catch (Exception)
+            {
+                respuesta=false;
+                throw;
+            }
+
+            return Json(new { resultado = respuesta }, JsonRequestBehavior.AllowGet);
+
+        }
+
+    }
+
+
+}
